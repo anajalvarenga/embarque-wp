92142128abe7b7f8637508f412c454f1d7ecdcd9
anajalvarenga <alvarengaanajulia6@gmail.com>
2021-03-23 11:41:55 -0300

    Adicionando Campos personalizados em internacionais

--- page-internacionais.php
+++ page-internacionais.php
@@ -15,188 +15,32 @@
                     </div>
                 </div>
             </div>
-            <div id="america">
+
+            <div id="continente">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2>Américas</h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4 p-0">
-                            <img src="img/1-1-americas.jpg" alt="Américas" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/1-2-americas.jpg" alt="Américas" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/1-3-americas.jpg" alt="Américas" class="p-2">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4">
-                            <div class="row p-2">
-                                <img src="img/2-1-americas.jpg" alt="Américas">
-                            </div>
-                            <div class="row p-2">
-                                <img src="img/3-1-americas.jpg" alt="Américas">
+                    <?php if(have_rows('continente')): while(have_rows('continente')) : the_row(); ?>
+                        <div class="row">
+                            <div class="col-12">
+                                <h2><?php the_sub_field('nome_continente'); ?></h2>
                             </div>
                         </div>
-                        <div class="col-8 p-0">
-                            <img src="img/big-americas.jpg" alt="Américas" class="p-2">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="europa">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2>Europa</h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4 p-0">
-                            <img src="img/1-1-europa.jpg" alt="Europa" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/1-2-europa.jpg" alt="Europa" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/1-3-europa.jpg" alt="Europa" class="p-2">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8 p-0">
-                            <img src="img/big-europa.jpg" alt="Europa" class="p-2">
-                        </div>
-                        <div class="col-4">
-                            <div class="row p-2">
-                                <img src="img/2-3-europa.jpg" alt="Europa">
+                        <?php if(have_rows('fotos_continente')): while(have_rows('fotos_continente')) : the_row(); ?>
+                            <div class="row">
+                                <div class="col-4 p-0">
+                                    <img src="<?php the_sub_field('foto_continente_1'); ?>" alt="<?php the_sub_field('nome_continente'); ?>" class="p-2">
+                                </div>
+                                <div class="col-4 p-0">
+                                    <img src="<?php the_sub_field('foto_continente_2'); ?>" alt="<?php the_sub_field('nome_continente'); ?>" class="p-2">
+                                </div>
+                                <div class="col-4 p-0">
+                                    <img src="<?php the_sub_field('foto_continente_3'); ?>" alt="<?php the_sub_field('nome_continente'); ?>" class="p-2">
+                                </div>
                             </div>
-                            <div class="row p-2">
-                                <img src="img/3-3-europa.jpg" alt="Europa">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="africa">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2>África</h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4">
-                            <div class="row p-2">
-                                <img src="img/1-1-africa.jpg" alt="África">
-                            </div>
-                            <div class="row p-2">
-                                <img src="img/2-1-africa.jpg" alt="África">
-                            </div>
-                        </div>
-                        <div class="col-8 p-0">
-                            <img src="img/big-africa.jpg" alt="África" class="p-2">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4 p-0">
-                            <img src="img/3-1-africa.jpg" alt="África" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/3-2-africa.jpg" alt="África" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/3-3-africa.jpg" alt="África" class="p-2">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="asia">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2>Ásia</h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-8 p-0">
-                            <img src="img/big-asia.jpg" alt="Ásia" class="p-2">
-                        </div>
-                        <div class="col-4">
-                            <div class="row p-2">
-                                <img src="img/1-3-asia.jpg" alt="Ásia">
-                            </div>
-                            <div class="row p-2">
-                                <img src="img/2-3-asia.jpg" alt="Ásia">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4 p-0">
-                            <img src="img/3-1-asia.jpg" alt="Ásia" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/3-2-asia.jpg" alt="Ásia" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/3-3-asia.jpg" alt="Ásia" class="p-2">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="oceania">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2>Oceania</h2>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4 p-0">
-                            <img src="img/1-1-oceania.jpg" alt="Oceania" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/1-2-oceania.jpg" alt="Oceania" class="p-2">
-                        </div>
-                        <div class="col-4 p-0">
-                            <img src="img/1-3-oceania.jpg" alt="Oceania" class="p-2">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-4">
-                            <div class="row p-2">
-                                <img src="img/2-1-oceania.jpg" alt="Oceania">
-                            </div>
-                            <div class="row p-2">
-                                <img src="img/3-1-oceania.jpg" alt="Oceania">
-                            </div>
-                        </div>
-                        <div class="col-8 p-0">
-                            <img src="img/big-oceania.jpg" alt="Oceania" class="p-2">
-                        </div>
-                    </div>
+                        <?php endwhile; else : endif; ?>
+                    <?php endwhile; else : endif; ?>
                 </div>
             </div>
 
-            <div id="contact-area">
-                <div class="container-fluid p-5">
-                    <div class="row mb-5">
-                        <div class="col-sm-2"></div>
-                        <div class="col-sm-8 col-md-6 col-xl-4 contact-box">
-                            <h2>Contate-nos</h2>
-                            <form action="">
-                                <input type="text" class="form-control" placeholder="Nome" name="name">
-                                <input type="email" class="form-control" placeholder="E-mail" name="email">
-                                <textarea class="form-control" rows="3" placeholder="Escreva aqui sua mensagem" name="massage"></textarea>
-                                <input type="submit" class="btn btn-primary" value="Enviar">
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </main>
     <?php endwhile; else: endif; ?>
     
